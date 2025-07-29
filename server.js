94b3c1be18888277039fb79912647a7763e809a3
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2025-07-29 01:45:17 -0400

    atualizando projeto

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ const dashboardRoutes = require("./routes/dashboardRoutes")
 
 const app = express()
 
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-)
+// app.use(
+//     cors({
+//         origin: process.env.CLIENT_URL || "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content-Type", "Authorization"],
+//     })
+// )
+app.use(cors({
+  origin: ['https://expense-frontend-sooty.vercel.app', 'http://localhost:5173'], 
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE'],
+}));
 
 app.use(express.json())
 
